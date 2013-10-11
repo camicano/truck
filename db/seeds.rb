522b961b0c82ffa7fa3127c08a98c492d2e0060b
Camila Cano <camilacanog@gmail.com>
2013-10-11 09:53:51 -0400

    add seeb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Truck.delete_all
+
+(1...20).each do |truck|
+	name = Faker::Commerce.product_name
+	food = Faker::Commerce.color
+
+	truck = Truck.create name: name, type_of_food: food 
+end
+
+
+
+
+
+# doc = Nokogiri::HTML(open('http://www.roaminghunger.com/ny'))
+# name = doc.css('.squarelist')
+
+
